78beefe9801ee0f651eac2555681e78ded05b82e
hypark023 <hypark023@192.168.26.42>
2022-12-26 14:15:09 +0900

    콩국물 레시피 커밋 -박혜연

---
+++ vegemil/src/main/java/com/vegemil/controller/BeanSoupRecipeController.java
@@ -0,0 +1,19 @@
+package com.vegemil.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BeanSoupRecipeController {
+
+	@GetMapping("/Main/beanSoupRecipe/{beansoupRecipe}")
+	public String getBeanSoupRecipePages(@PathVariable("beansoupRecipe") String beansoupRecipe) {
+		if(beansoupRecipe.contains(".")) {
+			beansoupRecipe = beansoupRecipe.substring(0, beansoupRecipe.lastIndexOf("."));
+		}
+		return "/beanSoupRecipe/" + beansoupRecipe;
+	}
+
+}
