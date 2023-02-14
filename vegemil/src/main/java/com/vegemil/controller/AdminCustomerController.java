98d201324e5618c960597f8bf6ce1e965f5992f6
hypark023 <hypark023@192.168.26.42>
2023-02-14 16:29:16 +0900

    웹 이미지 호출 함수 수정

--- vegemil/src/main/java/com/vegemil/controller/AdminCustomerController.java
+++ vegemil/src/main/java/com/vegemil/controller/AdminCustomerController.java
@@ -387,6 +387,24 @@ public class AdminCustomerController extends UiUtils {
 		return new ResponseEntity<Resource>(resource, header, HttpStatus.OK);
 	}
 	
+	//정적 이미지 불러오기
+	@GetMapping("/web/upload/{foldername}/{filename}")
+	public ResponseEntity<Resource> display(@PathVariable(value = "filename", required = false) String filename,
+											@PathVariable(value = "foldername", required = false) String foldername) {
+		Resource resource = new FileSystemResource(uploadPath + "/upload/"+ foldername + "/" + filename);
+		if(!resource.exists()) 
+			return new ResponseEntity<Resource>(HttpStatus.NOT_FOUND);
+		HttpHeaders header = new HttpHeaders();
+		Path filePath = null;
+		try {
+			filePath = Paths.get(uploadPath + "/upload/"+ foldername + "/" + filename);
+			header.add("Content-type", Files.probeContentType(filePath));
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity<Resource>(resource, header, HttpStatus.OK);
+	}
+	
 	@RequestMapping(value = "/admin/manage/customer/visit")
 	public String openVisit(@ModelAttribute("params") AdminVisitSetupDTO params, Model model,
 			@RequestParam Map<String, Object> commandMap) {
