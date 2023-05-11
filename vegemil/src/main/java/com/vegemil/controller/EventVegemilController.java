3f205e368c4817aeb6c4f561d0db58a6f38b6f7d
hypark023 <hypark023@192.168.26.42>
2023-05-11 10:40:51 +0900

    정식품 창립 50주년 이벤트 관련 추가 업데이트 - 박혜연

--- vegemil/src/main/java/com/vegemil/controller/EventVegemilController.java
+++ vegemil/src/main/java/com/vegemil/controller/EventVegemilController.java
@@ -1,34 +1,10 @@
 package com.vegemil.controller;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import com.vegemil.domain.BeansoupDTO;
-import com.vegemil.domain.BeansoupEventDTO;
-import com.vegemil.domain.BeansoupNewsDTO;
-import com.vegemil.domain.BeansoupVideoDTO;
-import com.vegemil.service.BeansoupService;
 import com.vegemil.util.UiUtils;
 
 @Controller
@@ -37,12 +13,9 @@ public class EventVegemilController extends UiUtils {
 	@Value("${spring.servlet.multipart.location}")
     private String uploadPath;
 	
-	/*
-	@GetMapping("/eventVegemil")
-	public String beanSoupBrand(Model model) {
-		
-		return "event/eventFor50years";
+	@GetMapping("/event/{eventTitle}")
+	public String beanSoupBrand(@PathVariable(value = "eventTitle", required = false) String eventTitle ) {
+		return "event/"+ eventTitle;
 	}
-	*/
 	
 }
