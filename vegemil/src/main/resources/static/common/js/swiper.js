7b8d580ea7abbaa1ea7b8dd8bd024a9d5498fb99
hypark023 <hypark023@gmail.com>
2024-02-15 18:02:39 +0900

    스와이퍼 코드 리팩토링(반복문으로 이벤트 걸기)

--- vegemil/src/main/resources/static/common/js/swiper.js
+++ vegemil/src/main/resources/static/common/js/swiper.js
@@ -1,5 +1,5 @@
 /*================================
- * 	 ºê·£µå > swiper
+ * 	 ï¿½ê·£ï¿½ï¿½ > swiper
  *================================*/
  $(document).ready(function(){
 	var swiper = new Swiper(".swiper-container", {
@@ -15,3 +15,76 @@
 	  mousewheel: true
 	});
 });
+
+
+/*================================
+ * 	 ï¿½ï¿½Ç°ï¿½ï¿½ï¿½ï¿½Æ® > ï¿½ï¿½ï¿½ï¿½ï¿½ > swiper
+ .listSwiper01 {}
+ .listSwiper02 {}
+ .listSwiper03 {}
+ .listSwiper04 {}
+ .listSwiper05 {}
+ .listSwiper06 {}
+ .listSwiper07 {}
+ .listSwiper08 {}
+ .listSwiper09 {}
+ .listSwiper10 {}
+ .listSwiper11 {}
+ *================================*/
+ $(document).ready(function(){
+	/*
+	  	var swiper = new Swiper(".listSwiper01", {
+		slidesPerView: 1,
+		spaceBetween: 30,
+		loop: true,
+		pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+		},
+		navigation: {
+		nextEl: ".listSwiper01-botton .swiper-button-next",
+		prevEl: ".listSwiper01-botton .swiper-button-prev",
+		},
+	});
+
+	var swiper = new Swiper(".listSwiper02", {
+		slidesPerView: 1,
+		spaceBetween: 30,
+		loop: true,
+		pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+		},
+		navigation: {
+		nextEl: ".listSwiper02-botton .swiper-button-next",
+		prevEl: ".listSwiper02-botton .swiper-button-prev",
+		},
+	});
+	
+	 */
+	 
+	var classNames = ".listSwiper0";
+	for(var i=0;i<40;i++) {
+		
+		if(i == 10) {
+			classNames = ".listSwiper";
+		}						
+		var swiper = new Swiper(classNames + i, {
+			slidesPerView: 1,
+			spaceBetween: 30,
+			loop: true,
+			pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		},
+			navigation: {
+			nextEl: classNames + i + "-botton .swiper-button-next",
+			prevEl: classNames + i + "-botton .swiper-button-prev",
+		},
+		});
+	}
+
+	 
+
+});
+
